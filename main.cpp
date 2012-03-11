5eb69757d8be069788cb84884bb2d44d93bc8237
Cgg <clement.geiger@gmail.com>
2012-03-11 19:07:19 +0100

    working with the qlist of qobjects method

--- main.cpp
+++ main.cpp
@@ -3,21 +3,39 @@
 
 #include "SimsProtocole/GestionClients.h"
 
+#include "FileReceivedModel.h"
+#include "downloadlistmodel.h"
+
+#include <QObject>
+#include <QDeclarativeContext>
 #include <QGraphicsObject>
+#include <QList>
 
 Q_DECL_EXPORT int main(int argc, char *argv[])
 {
     QScopedPointer<QApplication> app(createApplication(argc, argv));
 
+    // VARIABLES
+    QList< QObject * > downloads;
+
+
+    // INIT PROTOCOL PART
+    GestionClients gestionClient(0);
+
+
+    // INIT QML PART
     QmlApplicationViewer viewer;
     viewer.setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
     viewer.setMainQmlFile(QLatin1String("qml/UI/CaptainAdhocUI.qml"));
     viewer.showExpanded();
 
-    QGraphicsObject * qmlRootObject = viewer.rootObject();
+    QGraphicsObject     * qmlRootObject = viewer.rootObject();
 
-    GestionClients gestionClient(0);
+    rootContext->setContextProperty( "downloadsModel", QVariant::fromValue( downloads ) );
 
+    //rootContext->setProperty( "downloadingFilesList", QVariant::fromValue( downloadingFilesList) );
+
+    // CONNECT ALL THE SIGNALS
 //    QObject::connect( &gestionClient,
 //                      SIGNAL( ClientDownloadSpeedUpdate( Client*,int ) ),
 //                      )
