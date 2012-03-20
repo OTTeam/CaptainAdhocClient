1f67e9f00dbc38bd7abe2ee69341ca2a1d3d3ec2
Kevin Vandel <vandel.kevin@gmail.com>
2012-03-20 09:54:16 +0100

    gestion du connard qui a pas de carte wifi...

--- Wifi/WifiConnection.cpp
+++ Wifi/WifiConnection.cpp
@@ -32,59 +32,68 @@ void WifiConnection::Connect()
         _manager->RegisterNotifications();
         QList<WifiInterface*> * interfaceList;
         interfaceList = _manager->GetInterfaces();
-        qDebug() << "Got" << interfaceList->count() << "interface(s) :";
-        foreach (WifiInterface * intface,*interfaceList)
+        int nbInterfaces = interfaceList->count();
+        qDebug() << "Got" << nbInterfaces << "interface(s) :";
+
+        if(nbInterfaces == 0) //Pas de carte WiFi (forcement c'est pas pratique pour notre programme...)
         {
-            qDebug() << "Interface :" << intface->GetName();
-            qDebug() << "Radio :" << ((intface->IsRadioOn())?"ON" : "OFF");
-            switch(intface->GetStatus())
-            {
-            case CONNECTED:
-                qDebug() << "Status : Connecté";
-                intface->DisconnectWifi();
-                break;
-            case FORMED:
-                qDebug() << "Status : En attente";
-                break;
-            case DISCONNECTED:
-                qDebug() << "Status : Déconnecté";
-                break;
-            }
+            emit ConnectionFail();
         }
-
-        _manager->DeleteInterfaceList(interfaceList);
-
-        QList<WifiNetwork*> * netList;
-        netList = _manager->GetNetworks();
-        qDebug() << "Got" << netList->count() << "network(s) :";
-        bool found =false;
-        foreach (WifiNetwork * net,*netList)
+        else
         {
-            qDebug() << "Network :" << net->GetSSID();
-            if (net->GetSSID() == ADHOC_SSID)
+            foreach (WifiInterface * intface,*interfaceList)
             {
-                _network = net;
+                qDebug() << "Interface :" << intface->GetName();
+                qDebug() << "Radio :" << ((intface->IsRadioOn())?"ON" : "OFF");
+                switch(intface->GetStatus())
+                {
+                case CONNECTED:
+                    qDebug() << "Status : Connecté";
+                    intface->DisconnectWifi();
+                    break;
+                case FORMED:
+                    qDebug() << "Status : En attente";
+                    break;
+                case DISCONNECTED:
+                    qDebug() << "Status : Déconnecté";
+                    break;
+                }
+            }
+
+            _manager->DeleteInterfaceList(interfaceList);
+
+            QList<WifiNetwork*> * netList;
+            netList = _manager->GetNetworks();
+            qDebug() << "Got" << netList->count() << "network(s) :";
+            bool found =false;
+            foreach (WifiNetwork * net,*netList)
+            {
+                qDebug() << "Network :" << net->GetSSID();
+                if (net->GetSSID() == ADHOC_SSID)
+                {
+                    _network = net;
+                    _network->RegisterNetworkNotifications();
+                    _network->Connect(ADHOC_PWD);
+                    found=true;
+                }
+            }
+
+            if (found)
+            {
+                netList->removeOne(_network);
+            }
+
+            _manager->DeleteNetworkList(netList);
+
+            if (!found)
+            {
+                _network = _manager->CreateWifi(ADHOC_SSID,ADHOC_PWD);
                 _network->RegisterNetworkNotifications();
-                _network->Connect(ADHOC_PWD);
-                found=true;
             }
+
+            connect(_network, SIGNAL(ConnectionStatusChanged(int)), this, SLOT(onConnectionStatusChanged(int)));
+            connect(_network, SIGNAL(ConnectionFail(int)), this, SIGNAL(ConnectionFail()));
         }
-
-        if (found)
-        {
-            netList->removeOne(_network);
-        }
-
-        _manager->DeleteNetworkList(netList);
-
-        if (!found)
-        {
-            _network = _manager->CreateWifi(ADHOC_SSID,ADHOC_PWD);
-            _network->RegisterNetworkNotifications();
-        }
-
-        connect(_network, SIGNAL(ConnectionStatusChanged(int)), this, SLOT(onConnectionStatusChanged(int)));
-        connect(_network, SIGNAL(ConnectionFail(int)), this, SIGNAL(ConnectionFail()));
     }
 }
 
